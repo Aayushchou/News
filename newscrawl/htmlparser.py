b15fdd3a9925639e0e2cb9b66a437bf1f8d1ea6d
Aayushchou <aayushchou@gmail.com>
2021-04-08 21:28:12 +0100

    reformatted script

--- newscrawl/htmlparser.py
+++ newscrawl/htmlparser.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 
 
 def main(url=None):
-    web_config = pd.read_csv('webconfig/webconfig.csv', index_col=0)
+    web_config = pd.read_csv("webconfig/webconfig.csv", index_col=0)
     domain = urlparse(url).netloc
     try:
         config = web_config.loc[domain, :].to_dict()
@@ -14,21 +14,25 @@ def main(url=None):
         print(f"Website {domain} is not currently supported: {e}")
         exit()
 
-    process = CrawlerProcess(settings={
-        "FEEDS": {
-            f"output/{domain.replace('.', '_')}.json": {"format": "json"}
-        },
-    })
+    process = CrawlerProcess(
+        settings={
+            "FEEDS": {f"output/{domain.replace('.', '_')}.json": {"format": "json"}},
+        }
+    )
 
-    process.crawl(GeneralSpider,
-                  **config,
-                  urls=[url])
+    process.crawl(GeneralSpider, **config, urls=[url])
     process.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--url", required=False, help="URL of the website we want to parse", default='https://www.fca.org.uk/news')
+    parser.add_argument(
+        "-u",
+        "--url",
+        required=False,
+        help="URL of the website we want to parse",
+        default="https://www.fca.org.uk/news",
+    )
     args = vars(parser.parse_args())
-    main(url=args.get('url'))
+    main(url=args.get("url"))
